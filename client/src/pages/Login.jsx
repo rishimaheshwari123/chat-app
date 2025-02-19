ddfd8ad8ab727a08f51e6a62cb84d0d814dc3ff8
Rishi <rishimaheshwari040@gmail.com>
2025-02-19 15:05:52 +0530

    add hide and show password

--- client/src/pages/Login.jsx
+++ client/src/pages/Login.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FaUser, FaLock } from "react-icons/fa"; // Import icons from react-icons
+import { FaUser, FaLock, FaEye, FaEyeSlash } from "react-icons/fa"; // Import icons
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { login } from "../service/operations/user";
@@ -7,12 +7,12 @@ import { login } from "../service/operations/user";
 const LoginForm = () => {
   const [email, setEmail] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
   const handleSubmit = async (e) => {
     e.preventDefault();
-    // Handle form submission logic here
     await login(email, password, navigate, dispatch);
     console.log("Email:", email);
     console.log("Password:", password);
@@ -26,7 +26,7 @@ const LoginForm = () => {
         </h1>
         <form onSubmit={handleSubmit}>
           <div className="mb-4 relative">
-            <FaUser className="absolute left-3 top-3 text-blue-500" />
+            <FaUser size={20} className="absolute left-3 top-3 text-blue-500" />
             <input
               type="email"
               value={email}
@@ -37,18 +37,28 @@ const LoginForm = () => {
             />
           </div>
           <div className="mb-6 relative">
-            <FaLock className="absolute left-3 top-3 text-purple-500" />
+            <FaLock
+              className="absolute left-3 top-3 text-purple-500"
+              size={20}
+            />
             <input
-              type="password"
+              type={showPassword ? "text" : "password"}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               placeholder="Password"
-              className="w-full p-3 pl-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 transition duration-300 ease-in-out"
+              className="w-full p-3 pl-10 pr-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 transition duration-300 ease-in-out"
               required
             />
+            <button
+              type="button"
+              className="absolute right-3 top-3 text-gray-500 hover:text-gray-700"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? <FaEyeSlash size={25} /> : <FaEye size={25} />}
+            </button>
           </div>
           <div className="flex text-blue-600 justify-between items-center mb-3">
-            <p>Not have account?</p>
+            <p>Not have an account?</p>
             <Link to={"/register"}>Register</Link>
           </div>
           <button
--- client/src/pages/Register.jsx
+++ client/src/pages/Register.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from "react";
-import { FaUser, FaLock, FaPhone } from "react-icons/fa";
+import { FaUser, FaLock, FaPhone, FaEyeSlash, FaEye } from "react-icons/fa";
 import { useDispatch } from "react-redux";
 import { Link, useNavigate } from "react-router-dom";
 import { register } from "../service/operations/user";
@@ -9,6 +9,8 @@ const Register = () => {
   const [email, setEmail] = useState("");
   const [contactNumber, setContactNumber] = useState("");
   const [password, setPassword] = useState("");
+  const [showPassword, setShowPassword] = useState(false);
+
   const dispatch = useDispatch();
   const navigate = useNavigate();
 
@@ -25,7 +27,7 @@ const Register = () => {
         </h1>
         <form onSubmit={handleSubmit}>
           <div className="mb-4 relative">
-            <FaUser className="absolute left-3 top-3 text-blue-500" />
+            <FaUser size={20} className="absolute left-3 top-3 text-blue-500" />
             <input
               type="name"
               value={name}
@@ -36,7 +38,7 @@ const Register = () => {
             />
           </div>
           <div className="mb-4 relative">
-            <FaUser className="absolute left-3 top-3 text-blue-500" />
+            <FaUser size={20} className="absolute left-3 top-3 text-blue-500" />
             <input
               type="email"
               value={email}
@@ -47,7 +49,10 @@ const Register = () => {
             />
           </div>
           <div className="mb-4 relative">
-            <FaPhone className="absolute left-3 top-3 text-blue-500" />
+            <FaPhone
+              size={20}
+              className="absolute left-3 top-3 text-blue-500"
+            />
             <input
               type="number"
               value={contactNumber}
@@ -58,15 +63,25 @@ const Register = () => {
             />
           </div>
           <div className="mb-6 relative">
-            <FaLock className="absolute left-3 top-3 text-purple-500" />
+            <FaLock
+              className="absolute left-3 top-3 text-purple-500"
+              size={20}
+            />
             <input
-              type="password"
+              type={showPassword ? "text" : "password"}
               value={password}
               onChange={(e) => setPassword(e.target.value)}
               placeholder="Password"
-              className="w-full p-3 pl-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 transition duration-300 ease-in-out"
+              className="w-full p-3 pl-10 pr-10 border border-gray-300 rounded-lg focus:outline-none focus:ring-2 focus:ring-purple-500 transition duration-300 ease-in-out"
               required
             />
+            <button
+              type="button"
+              className="absolute right-3 top-3 text-gray-500 hover:text-gray-700"
+              onClick={() => setShowPassword(!showPassword)}
+            >
+              {showPassword ? <FaEyeSlash size={25} /> : <FaEye size={25} />}
+            </button>
           </div>
           <div className="flex text-blue-600 justify-between items-center mb-3">
             <p>Already have account?</p>
@@ -76,7 +91,7 @@ const Register = () => {
             type="submit"
             className="w-full bg-gradient-to-r from-blue-500 to-purple-600 text-white py-3 rounded-lg hover:from-blue-600 hover:to-purple-700 focus:outline-none focus:ring-2 focus:ring-blue-500 transition duration-300 ease-in-out"
           >
-            Login
+            Register
           </button>
         </form>
       </div>
